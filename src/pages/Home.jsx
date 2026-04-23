888431f01c87aa7c8311921dfdaf7739bf4be973
ShirM2 <shirmalkas123@gmail.com>
2026-04-23 23:10:38 +0300

    feat: implement initial home page structure and components
    - Added site navigation header
    
    - Created About section with custom animated icon and styling
    
    - Set up adoption mini-gallery preview structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar';
 const Home = () => {
     return(
 
-    <div className="min-h-screen bg-white">
+    <main className="min-h-screen bg-white">
       
       <Navbar />
       
@@ -23,16 +23,53 @@ const Home = () => {
         </div>
       </header>
 
-      <main className="max-w-6xl mx-auto p-6">
-        
-        {/* <div className="bg-orange-50 p-8 rounded-2xl border border-orange-100">
-          <h2 className="text-2xl font-bold mb-4">הארנבים שלנו</h2>
-          <p>בקרוב כאן... גלריית הארנבים המחכים לבית!</p>
-        </div> */}
-        
-      </main>
+      {/* אודות העמותה */}
+      <div className="flex flex-col items-center bg-[#F9FAFB] w-full h-auto text-center p-20 gap-3">
+        {/* אייקון */}
+        {/* mix-blend-multiply - מעלים את הרקע הלבן של הגיף וממזג אותו עם הרקע של האתר */}
+        {/* hue-rotate-[210deg] - משנה את גוון התכלת של האייקון לצבע הכתום כדי להתאים למיתוג העמותה */}
+        <img className="w-25 h-25 mix-blend-multiply hue-rotate-[210deg]" src="src/photos/icon.gif"></img>
+       
+        <h1 className="text-4xl text-black">על בית  מתנאל</h1>
+    
+        <div className="max-w-2xl mx-auto px-6 md:px-12">
+          <span className="text-center"> 
+            עמותת בית מתנאל הוקמה מתוך אהבה עמוקה לחיות ורצון לעזור להן.
+             אנחנו מחלצים חיות מחמד במצוקה. מספקים להן טיפול וטרינרי מלא,
+             מעניקים להן מקום בטוח להתאושש,ועובדים למצוא להן משפחות אוהבות ואחראיות לכל החיים.
+          </span>
+        </div>
 
-    </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full max-w-4xl mx-auto mt-12">
+          <div>
+            <h1 className="text-yellow-600 text-3xl">300+</h1>
+            <h2>חיות שחולצו</h2>
+          </div>
+
+          <div>
+            <h1 className="text-yellow-600 text-3xl">250+</h1>
+            <h2> אימוצים מוצלחים</h2>
+          </div>
+
+          <div>
+            <h1 className="text-yellow-600 text-3xl">24/7</h1>
+            <h2> זמינות לחירום</h2>
+          </div>
+        </div>
+      </div>
+
+      {/* מיני גלריה */}
+      <div className="flex flex-col items-center w-full h-auto text-center p-20 gap-3">
+        <h1 className="text-3xl">חיות מחמד מחכות לאימוץ</h1>
+        <h2>הכירו את חלק מחיות המחמד המקסימות שמחכות למשפחה חדשה</h2>
+
+        {/* Grids-הגלרייה עצמה תהיה מחולקת ל */}
+        <div className="grid grid-col-1 md:grid-col-3">
+          {/* קופוננטה של הכרטיסיות */}
+        </div>
+      </div>
+
+    </main>
   );
 };
 
