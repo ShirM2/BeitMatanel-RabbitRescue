466af861e8f4cd1e2c94eb78d883255db681135a
ShirM2 <shirmalkas123@gmail.com>
2026-04-23 18:16:12 +0300

    style: improve navbar responsiveness and create home page header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,28 +2,28 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav className="bg-[#F0EAD4] border-b border-gray-100 sticky top-0 z-50">
-      <div className="max-w-9xl mx-auto px-6 py-4 flex items-center relative">
+    <nav className="bg-[#F0EAD4] sticky top-0 z-50">
+      <div className="max-w-9xl mx-auto px-6 py-4 flex items-center justify-between relative">
         {/* לוגו האתר */}
-        <div className="flex-1 flex items-center gap-3">
-          <img src="/src/photos/logo.png" alt="לוגו" className="h-25 w-25" />
+        <div className="flex-1 flex items-center gap-2 md:gap-3">
+          <img src="/src/photos/logo.png" alt="לוגו" className="h-16 w-16 md:h-20 md:w-20" />
           <div className="text-xl font-bold text-[#E8A36A]">בית מתנאל</div>
         </div>
 
         {/* קישורי ניווט */}
-        <div className="flex-1 flex gap-10 justify-center">
-          <a href="/" className="whitespace-nowrap text-gray-700 hover:text-orange-300 font-medium transition-colors">דף הבית</a>
-          <a href="/animals" className="whitespace-nowrap text-gray-700 hover:text-orange-300 font-medium transition-colors">החיות שלנו</a>
-          <a href="/report" className="whitespace-nowrap text-gray-700 hover:text-orange-300 font-medium transition-colors">דווח על חיה במצוקה</a>
-          <a href="/donations" className="whitespace-nowrap text-gray-700 hover:text-orange-300 font-medium transition-colors">תרומות</a>
-          <a href="/about" className="whitespace-nowrap text-gray-700 hover:text-orange-300 font-medium transition-colors">אודות</a>
-          <a href="/contact" className="whitespace-nowrap text-gray-700 hover:text-orange-300 font-medium transition-colors">צור קשר</a>
+        <div className="flex-1 flex gap-3 xl:gap-10 justify-center">
+          <a href="/" className="whitespace-nowrap text-gray-700 hover:text-orange-300 font-medium transition-colors text-xs md:text-sm lg:text-base">דף הבית</a>
+          <a href="/animals" className="whitespace-nowrap text-gray-700 hover:text-orange-300 font-medium transition-colors text-xs md:text-sm lg:text-base">החיות שלנו</a>
+          <a href="/report" className="whitespace-nowrap text-gray-700 hover:text-orange-300 font-medium transition-colors text-xs md:text-sm lg:text-base">דווח על חיה במצוקה</a>
+          <a href="/donations" className="whitespace-nowrap text-gray-700 hover:text-orange-300 font-medium transition-colors text-xs md:text-sm lg:text-base">תרומות</a>
+          <a href="/about" className="whitespace-nowrap text-gray-700 hover:text-orange-300 font-medium transition-colors text-xs md:text-sm lg:text-base">אודות</a>
+          <a href="/contact" className="whitespace-nowrap text-gray-700 hover:text-orange-300 font-medium transition-colors text-xs md:text-sm lg:text-base">צור קשר</a>
         </div>
 
         {/* קישור לאימוץ */}
         <div className="flex-1 flex justify-center">
            <a href="/adpot_form" 
-              className="bg-[#E8A36A] text-white px-6 py-2 rounded-full font-bold shadow-md hover:bg-[#d48e55] transition-all duration-300 whitespace-nowrap">אני רוצה לאמץ</a>
+              className="bg-[#E8A36A] text-white px-3 py-1.5 md:px-6 md:py-2 rounded-full font-bold shadow-md hover:bg-[#d48e55] transition-all duration-300 whitespace-nowrap text-xs md:text-base">אני רוצה לאמץ</a>
         </div>
       </div>
     </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,9 +8,19 @@ const Home = () => {
       
       <Navbar />
       
-      <header className="py-10 text-center">
-        {/* <h1 className="text-5xl font-extrabold text-orange-600">בית מתנאל</h1> */}
-        
+      <header className="relative text-center">
+      
+        <img src='src/photos/header.png' className="w-full h-auto"></img>
+
+        <div className="absolute inset-0 flex flex-col justify-center items-center pb-43">
+          <h1 className="text-6xl text-white drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)]">מצילים,מטפלים ומאמצים חיות מחמד בישראל</h1>
+          <p className="mt-3 text-2xl text-white drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)]">עמותת בית מתנאל מחלצת ומשקמת חיות מחמד במצוקה ומוצאת להם בתים אוהבים</p>
+          <a href="/adpot_form" 
+              className="mt-3 bg-white text-yellow-500 px-3 py-1.5 md:px-6 md:py-2 rounded-full font-bold shadow-md hover:bg-yellow-100 transition-all duration-300 whitespace-nowrap text-xs md:text-base">אני רוצה לאמץ</a>
+
+          <a href="/report" 
+              className="mt-3 bg-red-700 text-yellow-500 px-3 py-1.5 md:px-6 md:py-2 rounded-full font-bold shadow-md hover:bg-red-800 transition-all duration-300 whitespace-nowrap text-xs md:text-base">דווח על חיה במצוקה⚠️</a>
+        </div>
       </header>
 
       <main className="max-w-6xl mx-auto p-6">
